8265fc110471b898641b1cdeae124bbabdb2d931
Andressa Lopes <andressalsmenezes@gmail.com>
2023-02-12 16:53:52 -0300

    Add: serializers

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from serializers import PetSerializer
+from models import Pet
 
-# Create your views here.
+
+class PetView(APIView, PageNumberPagination):
+    def post(self, request):
+        """
+        Aqui vou ter que criar o Group e as traits, mas caso o usuário tente inserir um dado de pet com grupo ou características que já estão salvas no banco de dados, não deve ser criado novamente o grupo ou a(s) característica(s) e sim devem ser utilizados os registros que já existem.
+        """
+        serializer = PetSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        pet = Pet.objects.create(**serializer.validated_data)
+        serializer = PetSerializer(pet)
+
+        return Response(serializer.data, 201)
+
+    def get(self, request):
+        pets = Pet.objects.all()
+        result_page = self.paginate_queryset(pets, request, view=self)
+        serializer = PetSerializer(result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
--- traits/models.py
+++ traits/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    created_at = models.CharField(max_length=10)
+    created_at = models.DateField()
     pets = models.ManyToManyField("pets.Pet", related_name="traits")
